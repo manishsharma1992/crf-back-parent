0ca632c585b6538e92026e4423f1b3f003feaff5
Manish Sharma <mssharmam51@gmail.com>
2026-07-31 00:02:47 +0530

    first commit

---
+++ crf-back/src/test/java/com/bnpparibas/sit/fresh/rds/rds04/crf/back/AppTest.java
@@ -0,0 +1,19 @@
+package com.bnpparibas.sit.fresh.rds.rds04.crf.back;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
---
+++ crf-datasynch/src/main/java/com/bnpparibas/sit/fresh/rds/rds04/crf/back/App.java
@@ -0,0 +1,10 @@
+package com.bnpparibas.sit.fresh.rds.rds04.crf.back;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
